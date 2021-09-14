4f7ce6807d432935692855aa0fd79592ea774237
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-09-14 11:04:40 +0200

    Int.story

---
+++ Interaktiv-story/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Console.Title = "Dungeon with only two floors?";
+
+Console.WriteLine("You enter the Dungeon and are met with two choices, Left or Right, Where do you go?");
+
+ string Go = Console.ReadLine();
+
+Go = Go.ToLower();
+
+
+
+if (Go == "right")
+{
+    Console.WriteLine("You walk towards the Right Hallway.");
+
+    
+}
+
+
+else if (Go == "left")
+{
+    Console.WriteLine("You walk towards the Left Hallway.");
+
+    
+}
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
