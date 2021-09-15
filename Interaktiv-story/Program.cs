188b6da593d8b213a6afedd984b3522de7af9e38
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-09-15 11:11:04 +0200

    INT.2

--- Interaktiv-story/Program.cs
+++ Interaktiv-story/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 
-Console.Title = "Dungeon with only two floors?";
+Console.Title = "Dungeon with only two paths.";
 
 Console.WriteLine("You enter the Dungeon and are met with two choices, Left or Right, Where do you go?");
 
- string Go = Console.ReadLine();
-
+string Go = Console.ReadLine();
 Go = Go.ToLower();
 
 
@@ -14,15 +13,115 @@ if (Go == "right")
 {
     Console.WriteLine("You walk towards the Right Hallway.");
 
-    
+    Console.WriteLine($"It's a dead end with a decaying ominous altar in the center created by the Umbra Witches, there is some text. Read or Ignore?");
+
+    string Do = Console.ReadLine();
+    Do = Do.ToLower();
+
+    if (Do == "read")
+    {
+        System.Console.WriteLine("You try reading the text, it is unintelligible. You lack the INT to comprehend. With nothing else to do you recall back. ");
+        Console.ReadLine();
+
+    }
+
+    if (Do == "ignore")
+    {
+        System.Console.WriteLine("You decide to ignore the text and continue looking around, after finding nothing else of interest you decide to recall back. ");
+        Console.ReadLine();
+
+
+
+    }
+    if (Do == "destroy")
+    {
+        System.Console.WriteLine("You destroy the Altar created by the Umbra church, the only thing left is a mysterious orb that was on the altar, take it? Yes or No.");
+
+        string Did = Console.ReadLine();
+        Did = Did.ToLower();
+
+        if (Did == "yes")
+        {
+            System.Console.WriteLine("After picking it up you hear a voice in your head saying, 'Your class has been forcefully changed to Lumen Sage.' Then you are sent back.");
+            Console.ReadLine();
+
+        }
+
+
+        else if (Did == "no")
+        {
+            System.Console.WriteLine("You find nothing else of interest and recall back.");
+            Console.ReadLine();
+
+        }
+
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 else if (Go == "left")
 {
     Console.WriteLine("You walk towards the Left Hallway.");
+    System.Console.WriteLine("It's a spacious room filled with flammable books stacked to the roof, there is a rough path through the books in the middle, Enter or Leave?");
 
+    string Walk = Console.ReadLine();
+    Walk = Walk.ToLower();
+
+    if (Walk == "enter")
+    {
+        System.Console.WriteLine("You mosey past the seemingly hundreds of books stacked up in tall towers, eventually reaching an open area with a poorly built chest. Open?");
+
+        string Open = Console.ReadLine();
+
+        if (Open == "Open")
+        {
+            System.Console.WriteLine("");
+            Console.ReadLine();
+        }
+        //make an if else = destroy
     
+
+    }
+    
+
+    if (Walk == "leave")
+    {
+        System.Console.WriteLine("You decide it's not worth the trouble and decide to leave, recalling back.");
+        Console.ReadLine();
+
+
+    }
+    else if (Walk == "burn them")
+    {
+        System.Console.WriteLine("You decide to set fire to the books, instantly creating an inferno in the room burning you to death. Stupid.");
+        Console.ReadLine();
+    }
+
+
+
+
+
+
+
+
+
 }
 
 
